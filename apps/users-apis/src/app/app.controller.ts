0188d14f8a364a6283078b80a199f085faceefdd
Tri Nguyen <shyssthanhtri@gmail.com>
2023-11-04 21:31:00 +0700

    feat: integrate users-apis with users-contracts

--- apps/users-apis/src/app/app.controller.ts
+++ apps/users-apis/src/app/app.controller.ts
@@ -1,13 +1,57 @@
 import { Controller, Get } from '@nestjs/common';
+import { UserContract } from '@taskify/users-contracts';
+import {
+  nestControllerContract,
+  NestControllerInterface,
+  NestRequestShapes,
+  TsRest,
+  TsRestRequest,
+} from '@ts-rest/nest';
 
 import { AppService } from './app.service';
 
+const c = nestControllerContract(UserContract);
+type RequestShapes = NestRequestShapes<typeof c>;
+
 @Controller()
-export class AppController {
+export class AppController implements NestControllerInterface<typeof c> {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @TsRest(c.signIn)
+  async signIn(@TsRestRequest() { body }: RequestShapes['signIn']) {
+    return {
+      status: 201 as const,
+      body: {
+        id: 1,
+        email: body.email,
+        token: '',
+      },
+    };
+  }
+  @TsRest(c.signUp)
+  async signUp(@TsRestRequest() { body }: RequestShapes['signUp']) {
+    return {
+      status: 201 as const,
+      body: {
+        id: 1,
+        email: body.email,
+      },
+    };
+  }
+  @TsRest(c.verify)
+  async verify(@TsRestRequest() { body }: RequestShapes['verify']) {
+    console.log({ body });
+    return {
+      status: 201 as const,
+      body: {
+        id: 1,
+        email: '',
+      },
+    };
   }
 }
--- apps/users-apis/src/main.ts
+++ apps/users-apis/src/main.ts
@@ -5,14 +5,27 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { UserContract } from '@taskify/users-contracts';
+import { generateOpenApi } from '@ts-rest/open-api';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = 'api/users';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT ?? 3000;
+
+  const document = generateOpenApi(UserContract, {
+    info: {
+      title: 'Users APIs',
+      version: '1.0.0',
+    },
+  });
+
+  SwaggerModule.setup([globalPrefix, 'docs'].join('/'), app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
